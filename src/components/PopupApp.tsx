96efa71d85620c767169cefce653d5e0e93a6d60
Davi Oliveira <davi.comp.oliveira@gmail.com>
2026-06-18 09:56:24 -0300

    fix: restaura ícone de metadados

--- src/components/PopupApp.tsx
+++ src/components/PopupApp.tsx
@@ -14,6 +14,7 @@ import {
   ReloadOutlined,
   RiseOutlined,
   StopOutlined,
+  UpOutlined,
 } from '@ant-design/icons'
 import { PopupPanelSkeleton } from './LoadingSkeletons'
 import type { ViolationsListState } from './ViolationsList'
